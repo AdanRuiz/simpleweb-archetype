27fa196693dcac041f7cfc258317f2549d0ca89b
taticohu <huhy1099@foxmail.com>
2022-03-13 16:16:03 +0800

    feat(init): init simpleweb-archetype project

---
+++ src/test/java/indi/huhy/archetypes/simpleweb/SimplewebApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.huhy.archetypes.simpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
